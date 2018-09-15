8f846c99c4c69bdbc436a05cb6e75a2c9f4c10bc
Eleven <ioswpf@gmail.com>
2018-09-15 14:25:32 +1200

    transfer code

---
+++ index.js
@@ -0,0 +1,38 @@
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+var kittenbot=require('./kittenbot');
+//const DATABASE_URL = "/Users/eleven/Library/Application Support/Postgres/var-10"
+
+const { Pool } = require('pg');
+const pool = new Pool({
+                      connectionString: process.env.DATABASE_URL,
+                      ssl: false
+                      });
+
+kittenbot();
+
+express()
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
+.get('/cool', (req, res) => res.send(cool()))
+.get('/db', async (req, res) => {
+     try {
+     const client = await pool.connect()
+     const results = await client.query('SELECT * FROM test_table');
+     console.log(results.rows)
+     res.render('pages/db', {results:results.rows})
+     client.release();
+     } catch (err) {
+     console.error(err);
+     res.send("Error " + err);
+     }
+     })
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+

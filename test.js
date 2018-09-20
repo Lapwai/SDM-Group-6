01d139c1def8b5adf3055bf6c4bbb5b624c7592c
Eleven <ioswpf@gmail.com>
2018-09-21 00:46:12 +1200

    ym & package

--- test.js
+++ test.js
@@ -29,3 +29,21 @@ console.log("test");
 //    });
 //  });
 //});
+
+
+
+
+'use strict';
+
+const User = require('./mochatest')
+const expect = require('chai').expect
+
+describe('#mochatest.js', () => {
+  describe('#mtest()', () => {
+    it('should export a function', () => {
+      expect(User.mtest).to.be.a('function')
+    })
+  })
+})
+
+console.log("test done")

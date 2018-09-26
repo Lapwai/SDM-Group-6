d8d8f65b8cac006069e2f6b62418dc253332774b
Eleven <ioswpf@gmail.com>
2018-09-27 05:20:02 +1200

    fix token

--- controllers/admincontroller.js
+++ controllers/admincontroller.js
@@ -49,7 +49,7 @@ exports.add = function(req, res) {
 
         // If the xoxp token was revoked, go to legacy tokens generate a new one.
         params.splice(0,1)
-        var url = 'https://slack.com/api/users.list?token=xoxp-434508566676-434711974866-433992924640-cdfcca1250e43a6ad13f0b1d7e490f66'
+        var url = 'https://slack.com/api/users.list?token=xoxp-434508566676-434711974866-445031849590-92048d3bd57bdc16595d6f1f271fc73f'
         request(url, { json: true }, (err, _, body) => {
             if(err || body['error']) {
                 res.send(err || body['error']);
--- routes/chatbot.js
+++ routes/chatbot.js
@@ -4,7 +4,7 @@ function chatbot() {
     var controller = Botkit.slackbot({debug: false})
     controller
     .spawn({
-           token: 'xoxb-434508566676-433992928064-YqMhX0D2iaeiRJvPEOeqL3L6' // Edit this line!
+           token: 'xoxb-434508566676-433992928064-p1kwW8EG1a1bocnj1cwSsVqi' // Edit this line!
            })
     .startRTM(function (err) {
               if (err) {

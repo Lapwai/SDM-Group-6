b8b0222580080b866b325dac4a65a4d5b8bb9778
bill zhang <laurafangfang@gmail.com>
2018-10-03 12:58:23 +1300

    /survey/list

--- controllers/surveycontroller.js
+++ controllers/surveycontroller.js
@@ -6,7 +6,7 @@ var textRes = require('./textresponse')
 
 // add a new survey
 exports.add = function(req, res) {
-    let role = verifyRole(req.body.user_id)
+    let role = verifyAuth(req.body.user_id)
     role.then(value => {
         textRes.textRes(res,false,value)
     }).catch(err => {
@@ -21,29 +21,73 @@ exports.set = function(req, res) {
 }
 
 // list all surveies which added by the researcher/manager
-exports.list = function(req, res) {
-
-
+exports.surveylistForResearcherOrManager = function(req, res) {
+    verifyAuth(req.body.user_id).then(value => {
+        var sql=processSqlwithRole(req.body.user_id,value)
+        queryAllSurveyWithId(sql).then(value =>{
+            textRes.textRes(res,false,value)
+        }).catch(err => {
+            textRes.textRes(res,true,err)
+        })
+    }).catch(err => {
+        textRes.textRes(res,true,err)
+    })
 }
 
 // view all feedbacks in terms of the survey
 exports.view = function(req, res) {
-
-
+   
+}
+exports.myHistory = function(req, res) {
+   
 }
 
-// verify role
-function verifyRole(user_id) {
+function processSqlwithRole(user_id,role){
+    if(role == 'manager'){
+        return 'SELECT * FROM survey WHERE user_id = \''+ user_id +'\';'
+    }else{
+        return 'SELECT * FROM survey'
+    }
+}
+module.exports={processSqlwithRole}
+//query all research from this person
+function queryAllSurveyWithId(sql) {
     return new Promise(function(resolve, reject) {
-        var results = db.pgQuery('SELECT role FROM roles WHERE user_id = \''+ user_id +'\';')
+        var results = db.pgQuery(sql)
         results.then( value => {
             if(value.rowCount === 0) {
-                reject('Does not find the member!')
+                reject('Sorry, no survey record for you.\n')
             } else {
-                resolve(value.rows[0]['role'])
+                resolve(listSurveyOnebyOne(value))
             }
-        }).catch(err => {
-            reject(err)
+        }).catch(error => {
+            reject(error)
+        })
+    })
+}
+
+function listSurveyOnebyOne(value){//List all survey one by one 
+    var output=''
+    for(var i=0;i<value.rowCount;i++){
+        output =output+[value.rows[i]['survey_id'],value.rows[i]['survey_name']]+'\n'
+    } 
+    return output
+}
+
+
+
+// verify role
+function verifyAuth(user_id) {
+    return new Promise(function(resolve, reject) {
+        var results = db.pgQuery('SELECT * FROM roles WHERE user_id = \''+ user_id +'\';')
+        results.then( value => {
+            if(value.rowCount === 0) {
+                reject('Sorry, only researcher and manager could list all their survey results.\n Use /my_history to check your history, please.')
+            } else {
+                resolve(value.rows[0]['role'])  
+            }
+        }).catch(error => {
+            reject(error)
         })
     })
 }
---
+++ tests/mochatest.js
@@ -0,0 +1,12 @@
+
+
+'use strict'
+
+
+function mtest(... rest) {
+
+    return 'a' + rest
+
+}
+
+module.exports = {mtest};

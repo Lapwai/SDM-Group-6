2be72a8a218b76a40097deb342de032031f880de
Eleven <ioswpf@gmail.com>
2018-10-15 06:51:47 +1300

    1 fix bug - title too long 2 fix bug - elements more than five

--- controllers/interactivitycontroller.js
+++ controllers/interactivitycontroller.js
@@ -67,108 +67,84 @@ function postConfDialog(trigger_id) {
 }
 function confAtt() {
     let attachments ={
-        "callback_id": "conf-dialog",
-        "title": "Notification configuration",
-        "state": "conf",
-        "elements": [
+        'callback_id': 'conf-dialog',
+        'title': 'Configuration',
+        'state': 'conf',
+        'elements': [
             {
-                "label": "Title",
-                "name": "title",
-                "type": "text",
-                "placeholder": "Just remind it is your time to submit your happiness information. Choose a button to click."
+                'label': 'Title',
+                'name': 'title',
+                'type': 'text',
+                'placeholder': 'Just remind it is your time to submit your happiness information. Choose a button to click.'
             },
             {
-                "label": "Start time",
-                "type": "select",
-                "name": "starttime",
-                "options": [
+                'label': 'Start time',
+                'type': 'select',
+                'name': 'starttime',
+                'options': [
                   {
-                    "label": "9:00",
-                    "value": "9"
+                    'label': '9:00',
+                    'value': '9'
                   },
                   {
-                    "label": "11:00",
-                    "value": "11"
+                    'label': '11:00',
+                    'value': '11'
                   },
                   {
-                    "label": "13",
-                    "value": "13:00"
+                    'label': '13',
+                    'value': '13:00'
                   },
                   {
-                    "label": "15:00",
-                    "value": "15"
+                    'label': '15:00',
+                    'value': '15'
                   }
                 ]
             },
             {
-                "label": "Option 1",
-                "name": "option 1",
-                "type": "text",
-                "placeholder": "Very Happy"
+                'label': 'Options',
+                'name': 'option',
+                'type': 'text',
+                'placeholder': 'Please input 5 levels such as \"Very Happy;Happy;Normal;Unhappy;Very Unhappy\" (separted by semicolon)'
             },
             {
-                "label": "Option 2",
-                "name": "option 2",
-                "type": "text",
-                "placeholder": "Happy"
-            },
-            {
-                "label": "Option 3",
-                "name": "option 3",
-                "type": "text",
-                "placeholder": "Normal"
-            },
-            {
-                "label": "Option 4",
-                "name": "option 4",
-                "type": "text",
-                "placeholder": "Unhappy"
-            },
-            {
-                "label": "Option 5",
-                "name": "option 5",
-                "type": "text",
-                "placeholder": "Very Unhappy"
-            },
-            {
-                "label": "Time interval",
-                "type": "select",
-                "name": "timeinterval",
-                "options": [
+                'label': 'Time interval',
+                'type': 'select',
+                'name': 'timeinterval',
+                'options': [
                     {
-                      "label": "2 Minutes",
-                      "value": "2"
+                      'label': '2 Minutes',
+                      'value': '2'
                     },
                     {
-                      "label": "3 Minutes",
-                      "value": "3"
+                      'label': '3 Minutes',
+                      'value': '3'
                     },
                     {
-                      "label": "4 Minutes",
-                      "value": "4"
+                      'label': '4 Minutes',
+                      'value': '4'
                     },
                     {
-                      "label": "5 Minutes",
-                      "value": "5"
+                      'label': '5 Minutes',
+                      'value': '5'
                     }
                 ]
             },
             {
-                "label": "Postpone time",
-                "type": "select",
-                "name": "postpone",
-                "options": [
+                'label': 'Postpone time',
+                'type': 'select',
+                'name': 'postpone',
+                'options': [
                     {
-                      "label": "5 Minutes",
-                      "value": "5"
+                      'label': '5 Minutes',
+                      'value': '5'
                     },
                     {
-                      "label": "10 Minutes",
-                      "value": "10"
+                      'label': '10 Minutes',
+                      'value': '10'
                     },
                     {
-                      "label": "15 Minutes",
-                      "value": "15"
+                      'label': '15 Minutes',
+                      'value': '15'
                     }
                 ]
             }
@@ -222,32 +198,32 @@ function interDialog(payload) {
 
 
 // {
-//     "type": "interactive_message",
-//     "actions": [
+//     'type': 'interactive_message',
+//     'actions': [
 //       {
-//         "name": "recommend",
-//         "value": "recommend",
-//         "type": "button"
+//         'name': 'recommend',
+//         'value': 'recommend',
+//         'type': 'button'
 //       }
 //     ],
-//     "callback_id": "comic_1234_xyz",
-//     "team": {
-//       "id": "T47563693",
-//       "domain": "watermelonsugar"
+//     'callback_id': 'comic_1234_xyz',
+//     'team': {
+//       'id': 'T47563693',
+//       'domain': 'watermelonsugar'
 //     },
-//     "channel": {
-//       "id": "C065W1189",
-//       "name": "forgotten-works"
+//     'channel': {
+//       'id': 'C065W1189',
+//       'name': 'forgotten-works'
 //     },
-//     "user": {
-//       "id": "U045VRZFT",
-//       "name": "brautigan"
+//     'user': {
+//       'id': 'U045VRZFT',
+//       'name': 'brautigan'
 //     },
-//     "action_ts": "1458170917.164398",
-//     "message_ts": "1458170866.000004",
-//     "attachment_id": "1",
-//     "token": "xAB3yVzGS4BQ3O9FACTa8Ho4",
-//     "original_message": {"text":"New comic book alert!","attachments":[{"title":"The Further Adventures of Slackbot","fields":[{"title":"Volume","value":"1","short":true},{"title":"Issue","value":"3","short":true}],"author_name":"Stanford S. Strickland","author_icon":"https://api.slack.comhttps://a.slack-edge.com/bfaba/img/api/homepage_custom_integrations-2x.png","image_url":"http://i.imgur.com/OJkaVOI.jpg?1"},{"title":"Synopsis","text":"After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."},{"fallback":"Would you recommend it to customers?","title":"Would you recommend it to customers?","callback_id":"comic_1234_xyz","color":"#3AA3E3","attachment_type":"default","actions":[{"name":"recommend","text":"Recommend","type":"button","value":"recommend"},{"name":"no","text":"No","type":"button","value":"bad"}]}]},
-//     "response_url": "https://hooks.slack.com/actions/T47563693/6204672533/x7ZLaiVMoECAW50Gw1ZYAXEM",
-//     "trigger_id": "13345224609.738474920.8088930838d88f008e0"
+//     'action_ts': '1458170917.164398',
+//     'message_ts': '1458170866.000004',
+//     'attachment_id': '1',
+//     'token': 'xAB3yVzGS4BQ3O9FACTa8Ho4',
+//     'original_message': {'text':'New comic book alert!','attachments':[{'title':'The Further Adventures of Slackbot','fields':[{'title':'Volume','value':'1','short':true},{'title':'Issue','value':'3','short':true}],'author_name':'Stanford S. Strickland','author_icon':'https://api.slack.comhttps://a.slack-edge.com/bfaba/img/api/homepage_custom_integrations-2x.png','image_url':'http://i.imgur.com/OJkaVOI.jpg?1'},{'title':'Synopsis','text':'After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy...'},{'fallback':'Would you recommend it to customers?','title':'Would you recommend it to customers?','callback_id':'comic_1234_xyz','color':'#3AA3E3','attachment_type':'default','actions':[{'name':'recommend','text':'Recommend','type':'button','value':'recommend'},{'name':'no','text':'No','type':'button','value':'bad'}]}]},
+//     'response_url': 'https://hooks.slack.com/actions/T47563693/6204672533/x7ZLaiVMoECAW50Gw1ZYAXEM',
+//     'trigger_id': '13345224609.738474920.8088930838d88f008e0'
 //   }

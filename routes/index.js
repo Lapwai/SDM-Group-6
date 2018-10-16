31d35b36436859a5e93489b84ba92567dfe2dcad
Eleven <ioswpf@gmail.com>
2018-10-17 02:18:46 +1300

    update feedbacks web page

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,12 @@ router.get('/csv', (req, res) => {
 
 function queryfeedbacks(res) {
   let selectSql = 'select * from feedbacks where option <> \'-1\';'
-  db.pgQuery(selectSql).then(results => {
-    res.render('feedbacks', {results:results.rows})
+  db.pgQuery(selectSql).then(temp => {
+    let results = []
+    temp.rows.forEach(e => {
+      results.push({'name':e.member_name,'time':e.ts.toISOString().replace(/T/, ' ').replace(/\..+/, ''),'option':e.option,'comment':e.comment})
+    });
+    res.render('feedbacks', {results:results})
   })
 }
 
